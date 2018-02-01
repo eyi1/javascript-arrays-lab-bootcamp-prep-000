cc14f0ad345308fc44b6fb8f2fa0afe13e722bb6
eyi1 <ej7729@gmail.com>
2018-02-01 01:06:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = "I don't do much."
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendKitten(name){
-  
+
 return kittens.push(name)
 }
 destructivelyAppendKitten('Ralph')
